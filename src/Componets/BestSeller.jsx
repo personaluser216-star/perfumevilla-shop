cf44e91f5592988a7ea9344d74ea38cb6c0b4233
personaluser216-star <personaluser216@gmail.com>
2025-10-04 12:23:37 +0530

    Update BestSeller.jsx

--- src/Componets/BestSeller.jsx
+++ src/Componets/BestSeller.jsx
@@ -19,7 +19,7 @@ const BestSeller = () => {
   };
 
   return (
-    <div className="py-10 bg-[#f8f7f4]">
+    <div className="md:py-10 bg-[#f8f7f4]">
       {/* Title */}
       <motion.div
         className="text-center md:text-3xl py-8"
@@ -29,14 +29,14 @@ const BestSeller = () => {
         transition={{ duration: 0.5 }}
         variants={fadeUp}
       >
-        <h1 className="font-bold text-[#5b4f47]">Shop Our Best Seller</h1>
+        <h1 className="text-2xl md:text-3xl font-bold text-[#5b4f47]">Shop Our Best Seller</h1>
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 pt-4 pb-4">
           Discover our best-selling clothes, where style meets comfort in every stitch.
         </p>
       </motion.div>
 
       {/* Products Grid */}
-      <div className="md:ml-32 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="md:ml-32 ml-4 mr-4  md:mr-32 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {bestseller.map((item, index) => (
           <motion.div
             key={index}
